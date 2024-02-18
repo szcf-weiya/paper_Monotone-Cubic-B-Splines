190eb2c6a5be1cc18058e0a12d60888f9cad6bff
szcf-weiya <szcfweiya@gmail.com>
2024-02-18 15:05:31 -0500

    mv lambdas to arguments

--- code/loss_ml.jl
+++ code/loss_ml.jl
@@ -11,7 +11,7 @@ import importlib
 importlib.reload(mlp)
 """
 
-function cpr_classic_mlp(; N = 100, nepoch = 10000, gpu_id = -1, ngrid = 100, nhidden = 100, eta = 1e-4)
+function cpr_classic_mlp(; N = 100, nepoch = 10000, gpu_id = -1, ngrid = 100, nhidden = 100, eta = 1e-4, λmin = 1e-6, λmax = 0.0)
     # https://github.com/szcf-weiya/MonotoneSplines.jl/blob/2b5310d6d8583f9c898fb9c9e5583eb842493f13/src/boot.jl#L105
     seed = 1
     n = 100
@@ -23,15 +23,12 @@ function cpr_classic_mlp(; N = 100, nepoch = 10000, gpu_id = -1, ngrid = 100, nh
     B, L, J = MonotoneSplines.build_model(x, prop_nknots = prop_nknots)
 
 
-    λs = 10 .^ (range(-6, 0, length = 10))
-    λmin = minimum(λs)
-    λmax = maximum(λs)
-    arr_λs = rand(N) * (λmax - λmin) .+ λmin
-    βs = [mono_ss(x, y, λ, prop_nknots = prop_nknots).β for λ in arr_λs]
+    λs = rand(N) * (λmax - λmin) .+ λmin
+    βs = [mono_ss(x, y, λ, prop_nknots = prop_nknots).β for λ in λs]
     grid_λs = range(λmin, λmax, length=ngrid)
     grid_βs = [mono_ss(x, y, λ, prop_nknots = prop_nknots).β for λ in grid_λs]
     βs_mat = vcat(βs'...)'
-    λs_mat = vcat(MonotoneSplines.aug.(arr_λs)'...)'
+    λs_mat = vcat(MonotoneSplines.aug.(λs)'...)'
     βs_grid_mat = vcat(grid_βs'...)'
     λs_grid_mat = vcat(MonotoneSplines.aug.(grid_λs)'...)'
 
